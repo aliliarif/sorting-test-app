45816b7708df83388a63957d1efbac68b2fc50bc
Arif Alili <arif.alili@Arif-Alili-MacBook-Pro.local>
2023-02-24 13:03:03 +0100

    chore: Refactor Sort Service
    - Calculation of field totals is now done in saparate function and calculation is performed for dynamic fields - which are defined on CalculableFields enum
    - Calculation of sorting score is now done in separate function.
    - Improvements on code formatting

--- src/sort/sort.service.ts
+++ src/sort/sort.service.ts
@@ -1,87 +1,115 @@
 import { Injectable } from '@nestjs/common';
-import { Product, AccumulatedData, ProductResponse, SortingFormula, Operators } from './types';
+import {
+  Product,
+  AccumulatedData,
+  ProductResponse,
+  Variant,
+  SortingFormula,
+  Operators,
+  CalculableFields,
+  VariantTotal,
+} from './types';
 @Injectable()
 export class SortService {
-	/**
-	 *
-	 * @param products Products (and their variants) to be sorted.
-	 * @param sortingFormula Sorting formula which the sorting algorithm needs to follow
-	 * @returns Sorted products and accumulated data per product
-	 */
-	sortProducts(products: Product[], sortingFormula: SortingFormula[]): ProductResponse[] {
-		let productsResponse = [] as ProductResponse[];
+  /**
+   *
+   * @param products Products (and their variants) to be sorted.
+   * @param sortingFormula Sorting formula which the sorting algorithm needs to follow
+   * @returns Sorted products and accumulated data per product
+   */
+  sortProducts(products: Product[], sortingFormula: SortingFormula[]): ProductResponse[] {
+    let productsResponse = [] as ProductResponse[];
 
-		for (const product of products) {
-			let productResponse: ProductResponse;
+    for (const product of products) {
+      let productResponse: ProductResponse;
 
-			// * Calculate accumulated data
-			// This calculation is done assuming that variant fields are not dynamic
-			// formulaForVariantField() is used to determine field calculation method
-			const accumulatedProductData: AccumulatedData = {
-				price: product.price,
-				...product.variants.reduce(
-					(acc, item) => {
-						const variantsCount = product.variants.length;
-						acc.stock +=
-							this.formulaForVariantField('stock') === 'arithmetic-mean'
-								? item.stock / variantsCount
-								: item.stock;
-						acc.pageView +=
-							this.formulaForVariantField('pageView') === 'arithmetic-mean'
-								? item.pageView / variantsCount
-								: item.pageView;
-						acc.conversionRate +=
-							this.formulaForVariantField('conversionRate') === 'arithmetic-mean'
-								? item.conversionRate / variantsCount
-								: item.conversionRate;
-						acc.refundRate +=
-							this.formulaForVariantField('refundRate') === 'arithmetic-mean'
-								? item.refundRate / variantsCount
-								: item.refundRate;
+      // * Calculate accumulated data
+      const accumulatedProductData: AccumulatedData = {
+        price: product.price,
+        ...this.calculateVariantFieldTotals(product.variants),
+      };
 
-						return acc;
-					},
-					{ stock: 0, pageView: 0, conversionRate: 0, refundRate: 0 }
-				),
-			};
+      // * Calculate sorting score of the product
+      accumulatedProductData.sortingScore = this.calculateSortingScore(
+        sortingFormula,
+        accumulatedProductData
+      );
 
-			// * Find sorting score for the product
-			// Sorting score is calculated based on sortingFormula passed as parameter
-			// Formula is based on operator (as Operators enum) and operand which is translated to variant field value
-			// With one exception, `fixedValue:X` is passed as operand
-			let sumScore = '';
-			for (const formula of sortingFormula) {
-				const operator = Operators[formula.operator];
-				const operand =
-					formula.name.split(':')[0] === 'fixedValue'
-						? formula.name.split(':')[1]
-						: accumulatedProductData[formula.name];
-				sumScore += `${operator} ${operand}`;
-			}
-			accumulatedProductData.sortingScore = eval(sumScore);
+      // * Form the response object
+      productResponse = { ...product, accumulatedProductData };
+      productsResponse.push(productResponse);
+    }
 
-			// * Form the response object
-			productResponse = { ...product, accumulatedProductData };
-			productsResponse.push(productResponse);
-		}
+    return productsResponse.sort(
+      (a, b) => b.accumulatedProductData.sortingScore - a.accumulatedProductData.sortingScore
+    );
+  }
 
-		return productsResponse.sort(
-			(a, b) => b.accumulatedProductData.sortingScore - a.accumulatedProductData.sortingScore
-		);
-	}
+  /**
+   * Calculates totals of variant fields that are eligible for calculation
+   * Uses formulaForVariantField() to determine the algorithm of calculation
+   * @param variants Product Variants
+   * @returns Totals of variant fields eligible for calculation
+   */
+  private calculateVariantFieldTotals(variants: Variant[]): VariantTotal {
+    const sumVariantTotals: VariantTotal = {};
 
-	private formulaForVariantField(fieldName) {
-		switch (fieldName) {
-			case 'stock':
-				return 'arithmetic-sum';
-			case 'pageView':
-				return 'arithmetic-mean';
-			case 'conversionRate':
-				return 'arithmetic-mean';
-			case 'refundRate':
-				return 'arithmetic-mean';
-			default:
-				return 'arithmetic-mean';
-		}
-	}
+    for (const variant of variants) {
+      const variantsCount = variants.length;
+      Object.keys(variant).forEach((variantFieldName) => {
+        if (Object.values(CalculableFields).includes(variantFieldName)) {
+          if (!sumVariantTotals?.[variantFieldName]) sumVariantTotals[variantFieldName] = 0;
+          sumVariantTotals[variantFieldName] +=
+            this.formulaForVariantField(variantFieldName) === 'arithmetic-mean'
+              ? variant[variantFieldName] / variantsCount
+              : variant[variantFieldName];
+        }
+      });
+    }
+
+    return sumVariantTotals;
+  }
+
+  /**
+   * Sorting score is calculated based on sortingFormula passed as parameter
+   * Formula is based on operator (as Operators enum) and operand which is translated to variant field value
+   * With one exception, `fixedValue:X` is passed as operand
+   * @param sortingFormula
+   * @param accumulatedProductData
+   * @returns Calculated Score for given Product
+   */
+  private calculateSortingScore(
+    sortingFormula: SortingFormula[],
+    accumulatedProductData: AccumulatedData
+  ): number {
+    let sumScore = '';
+    for (const formula of sortingFormula) {
+      const operator = Operators[formula.operator];
+      const operand =
+        formula.name.split(':')[0] === 'fixedValue'
+          ? formula.name.split(':')[1]
+          : accumulatedProductData[formula.name];
+      sumScore += `${operator} ${operand}`;
+    }
+
+    // safe to use "eval" here because both operators and operand are assumed to be "safe" variables
+    // eval follows PEMDAS by default
+    // otherwise, if eval got to be switched for another solution, calculator service should be created which will follow PEMDAS
+    return eval(sumScore);
+  }
+
+  private formulaForVariantField(fieldName) {
+    switch (fieldName) {
+      case 'stock':
+        return 'arithmetic-sum';
+      case 'pageView':
+        return 'arithmetic-mean';
+      case 'conversionRate':
+        return 'arithmetic-mean';
+      case 'refundRate':
+        return 'arithmetic-mean';
+      default:
+        return 'arithmetic-mean';
+    }
+  }
 }
